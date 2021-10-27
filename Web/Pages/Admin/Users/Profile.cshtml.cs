3f7fd65ba85577aa5ef1f3cb81dd5de781507ff3
Wilcris Cortes <wilcris.cortes@mindweb.com.ph>
2021-10-27 10:02:09 +0800

    Update to Profile and Settings

---
+++ Web/Pages/Admin/Users/Profile.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Web.Pages.Admin.Users
+{
+    public class ProfileModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
