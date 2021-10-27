884ab2a423b6ef4c4a01b15971a6064c39bce97e
Wilcris Cortes <wilcris.cortes@mindweb.com.ph>
2021-10-27 11:03:00 +0800

    Ajax GET Table Implementation

--- Web/Pages/TicketApp/Index.cshtml.cs
+++ Web/Pages/TicketApp/Index.cshtml.cs
@@ -9,10 +9,28 @@ namespace Web.Pages.TicketApp
 {
     public class IndexModel : PageModel
     {
+        private readonly Infrastructure.Data.AppDbContext _context;
+
+        public IndexModel(Infrastructure.Data.AppDbContext context)
+        {
+            _context = context;
+        }
+
         public void OnGet()
         {
         }
 
+        public PartialViewResult OnGetViewTicketPartial()
+        {
+            var ticketList = _context.Tickets.AsEnumerable();
+
+            return new PartialViewResult
+            {
+                ViewName = "_ViewTicket",
+                ViewData = new Microsoft.AspNetCore.Mvc.ViewFeatures.ViewDataDictionary<IEnumerable<Core.Entities.Ticket>>(ViewData, ticketList)
+            };
+
+        }
 
     }
 }
